ec4f5c48e34b7736ddff50717f9fac541b5d025c
agurec <aboba@gmail.com>
2023-09-30 19:32:13 +0300

    first pyton script

---
+++ file.py
@@ -0,0 +1,16 @@
+print(5678)
+print(5+5)
+print(6+9+9)
+print(6666-888)
+print(10-20)
+print(type(5)) # int - целые числа
+print("Терминал")
+print('Ночной дозор')
+print('Я посмотрел фильм "Барби"')
+print('Hello' + 'World') # конкрентация
+
+print(type("строка"))
+
+login = input("Введите логин: ")
+password = input("Введите пароль ")
+print("Вы ввели " login + login ", " + password)
