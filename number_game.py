27fa97208f5cee87b5bb0d5cec5a763ffb2d9e58
Arsenii7777Pro <145859433+Arsenii7777Pro@users.noreply.github.com>
2023-11-11 16:31:32 +0300

    Add files via upload

---
+++ number_game.py
@@ -0,0 +1,23 @@
+import random
+pl_name = input("Введите  ваше имя")
+print(f"{pl_name} добро пожаловать в игру :")
+print("Компьютер загадал число от 1 до 100.попоробуй отгодать")
+comp_number = random.randint(1, 100) 
+tries = 11
+while tries <= 10:
+      tries -= 1   
+      print(f"Попытка №{tries}")
+      my_number = int(input(f"{pl_name}Введите число"))
+
+         
+      if my_number > comp_number:
+        print(f"{pl_name}загаданое число меньше ")
+      elif my_number < comp_number:
+        print("Загаданое число больше")
+      elif my_number == comp_number:
+        print(f"{pl_name}Вот ваш миллион доларов")
+        break
+      if tries == 0:
+        print(f"Правильный ответ - {comp_number}. {pl_name} проииграл все имущесттво")
+        break
+       
